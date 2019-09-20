9f1609068a73b73bd441203737a65a7589aa5aad
Victor Hugo <victor.hugoam99@gmail.com>
2019-09-20 17:32:20 -0300

    exercicio20/09

---
+++ exercicio20.09/09/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            do {
+            
+            Console.WriteLine("Digite um número:");
+            num1 = int.Parse(Console.ReadLine());
+
+            if (num1 % 2 != 0){
+
+                Console.WriteLine(" Seu número é impar.");
+                
+            } else {
+                Console.WriteLine(" Seu número é par.");
+            }
+        }while (num1 != 0);
+            
+            
+        }
+    }  
+}   
---
+++ exercicio20.09/imparInt/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace imparInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for ( int n = 1; n <= 100; n += 2){
+                Console.Write(n + " ");
+            }
+        }
+    }
+}
