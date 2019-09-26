c358b7043318b045db56b0756269de3e985e11fc
Victor Hugo <victor.hugoam99@gmail.com>
2019-09-26 17:21:16 -0300

    Ex26/09

---
+++ Vetor01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace matriz {
+   class Program {
+       static void Main (string[] args) {
+           int[] vetor = new int[6];
+           int k = 0;
+           int j = 0;
+           for (int i = 0; i <= 5; i++) {
+               Console.Write ("Digite o " + (i + 1) + "º número: ");
+               vetor[i] = int.Parse (Console.ReadLine ());
+               if (vetor[i] % 2 != 0)
+               {
+                   k ++ ;
+               } else
+               {
+                   ++ j;
+               }
+           }
+               Console.WriteLine(k + " Números ímpares");
+               Console.WriteLine(j + " Números pares");
+       }
+   }
+}
